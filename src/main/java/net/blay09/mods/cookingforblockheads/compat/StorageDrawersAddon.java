5a8974c2b3c3397f790197931c47d20cd4ac0fee
dibby <dibby@do.da>
2025-04-15 20:46:53 +1000

    Make DrawerController kitchen compatible

--- src/main/java/net/blay09/mods/cookingforblockheads/compat/StorageDrawersAddon.java
+++ src/main/java/net/blay09/mods/cookingforblockheads/compat/StorageDrawersAddon.java
@@ -1,9 +1,6 @@
 package net.blay09.mods.cookingforblockheads.compat;
 
 import net.blay09.mods.cookingforblockheads.KitchenMultiBlock;
-import net.blay09.mods.cookingforblockheads.api.kitchen.IKitchenStorageProvider;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.MinecraftForge;
 
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -12,8 +9,10 @@ public class StorageDrawersAddon {
 
     public StorageDrawersAddon() {
         KitchenMultiBlock.tileEntityWrappers
-                .put("com.jaquadro.minecraft.storagedrawers.block.tile.TileEntityDrawersStandard", DrawerWrapper.class);
                 .put("com.jaquadro.minecraft.storagedrawers.block.tile.TileEntityDrawersStandard", SimpleStorageProvider.class);
+        KitchenMultiBlock.tileEntityWrappers
+                .put("com.jaquadro.minecraft.storagedrawers.block.tile.TileEntityController", SimpleStorageProvider.class);
+
         KitchenMultiBlock.registerConnectorBlock(GameRegistry.findBlock("StorageDrawers", "trim"));
         MinecraftForge.EVENT_BUS.register(this);
     }
